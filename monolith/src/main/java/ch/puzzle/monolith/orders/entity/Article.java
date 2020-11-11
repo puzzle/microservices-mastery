48001b01b815db1aad66258369f985abc1046d30
rhertle <hertle@puzzle.ch>
2020-11-11 15:11:36 +0100

    Add liquibase and more dependencies

--- monolith/src/main/java/ch/puzzle/monolith/orders/entity/Article.java
+++ monolith/src/main/java/ch/puzzle/monolith/orders/entity/Article.java
@@ -9,4 +9,28 @@ public class Article extends PanacheEntity {
 
     private String name;
     private Double price;
+
+    public Article() {
+    }
+
+    public Article(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
--- monolith/src/main/java/ch/puzzle/monolith/orders/entity/ShopOrder.java
+++ monolith/src/main/java/ch/puzzle/monolith/orders/entity/ShopOrder.java
@@ -12,4 +12,28 @@ public class ShopOrder extends PanacheEntity {
 
     @Enumerated(EnumType.STRING)
     private ShopOrderStatus status;
+
+    public ShopOrder() {
+    }
+
+    public ShopOrder(List<Article> articles, ShopOrderStatus status) {
+        this.articles = articles;
+        this.status = status;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public ShopOrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ShopOrderStatus status) {
+        this.status = status;
+    }
 }
