41d5b4421b8e66d46c75ca50e5bd075e3a09e13e
rhertle <hertle@puzzle.ch>
2020-11-11 15:20:22 +0100

    Update docker-compose for monolith

--- monolith/src/test/java/ch/puzzle/monolith/orders/boundary/NativeOrderResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ch.puzzle.monolith.orders.boundary;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeOrderResourceIT extends OrderResourceTest {
-
-    // Execute the same tests but in native mode.
-}
