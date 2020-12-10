5bfff1cdbd6ef2dfe4a9386de7c0bad4a0faf482
rhertle <hertle@puzzle.ch>
2020-12-10 10:54:07 +0100

    Apply changes from debezium module to ShopOrderService

--- kafka/mm-kafka-order/src/main/java/ch/puzzle/kafka/order/control/ShopOrderService.java
+++ kafka/mm-kafka-order/src/main/java/ch/puzzle/kafka/order/control/ShopOrderService.java
@@ -1,21 +1,21 @@
 package ch.puzzle.kafka.order.control;
 
+import ch.puzzle.kafka.article.entity.Article;
 import ch.puzzle.kafka.order.entity.ShopOrder;
 import ch.puzzle.kafka.order.entity.ShopOrderDTO;
+import ch.puzzle.kafka.order.entity.ShopOrderStatus;
 import ch.puzzle.kafka.stock.boundary.ArticleStockRequestProducer;
-import ch.puzzle.kafka.stock.entity.ArticleStockRequest;
 import org.eclipse.microprofile.opentracing.Traced;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class ShopOrderService {
 
     @Inject
-    @RestClient
     ArticleStockRequestProducer articleStockRequestProducer;
 
     public List<ShopOrder> listAll() {
@@ -24,21 +24,18 @@ public class ShopOrderService {
 
     @Traced
     public ShopOrder createOrder(ShopOrderDTO shopOrderDTO) {
+        List<Long> ids = shopOrderDTO.articleOrders.stream().map(s -> s.articleId).collect(Collectors.toList());
+        List<Article> articles = Article.list("id in ?1", ids);
+
+        // store order to shopOrder table
         ShopOrder shopOrder = new ShopOrder();
+        shopOrder.setStatus(ShopOrderStatus.NEW);
+        shopOrder.setArticles(articles);
+        shopOrder.persist();
+
+        // fire event
         articleStockRequestProducer.createRequest(shopOrderDTO);
 
-//        if(stockResponse == null || stockResponse.getStatus() < 200 || stockResponse.getStatus() >= 300) {
-//            throw new WebApplicationException("Exception in ArticleStockService. HttpResponseCode: " + stockResponse.getStatus());
-//        }
-//
-//        List<Article> articles = new ArrayList<>();
-//        for (ArticleOrderDTO articleOrder : shopOrderDTO.articleOrders) {
-//            Object article = Article.findById(articleOrder.articleId);
-//            articles.add((Article) article);
-//        }
-//
-//        shopOrder.setArticles(articles);
-//        shopOrder.setStatus(ShopOrderStatus.NEW);
         return shopOrder;
     }
 }
